084a292c208c383a0a138b99ca6a45b23cfa9bf9
Alice <68032832+SplashyFish14@users.noreply.github.com>
2023-02-17 19:49:35 +0000

    Initial Commit

---
+++ MEngProject/MEngProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEngProjectApp.swift
+//  MEngProject
+//
+//  Created by Alice Milburn on 17/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct MEngProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
